0a024a5ca3ad899dae2f6d0d8d0109cbff375ae9
Daniel Leite <daniel.f.m.leite@gmail.com>
2021-06-28 07:59:58 +0100

    add like function

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -9,8 +9,9 @@ export default (posts = [], action) => {
             return [...posts, action.payload]
 
         case 'UPDATE':
-            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));   
-
+        case 'LIKE':
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));  
+        
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload)
       
